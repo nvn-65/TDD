149f40999ae6e1dac7e352be1a22b3727ebabb7b
Vladimir <nvn-65@mail.ru>
2021-11-13 18:58:24 +0300

    ФТ теперь проверяет возможность ввести элемент списка.

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,5 +1,10 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
+
+options = webdriver.ChromeOptions()
+binary_yandex_driver_file = 'yandexdriver.exe'  # path to YandexDriver
 
 
 class NewVisitorTest(unittest.TestCase):
@@ -7,7 +12,8 @@ class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
         """установка"""
-        self.browser = webdriver.Firefox()
+        self.browser = webdriver.Chrome(binary_yandex_driver_file, options=options)
+
 
     def tearDown(self):
         """демонтаж"""
@@ -22,17 +28,29 @@ class NewVisitorTest(unittest.TestCase):
         # Она видит, что заголовок и шапка страницы говорят о списках
         # неотложных дел
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу же предлагается ввести элемент списка
+        inputbox = self.browser.find_element('id_new_item')
+        self.assertEqual(inputbox.get_attribute('placeholder'), 'Введите задачу')
         # Она набирает в текстовом поле "Купить павлиньи перья" (ее хобби –
         # вязание рыболовных мушек)
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element('id_list_table')
+        rows = table.find_elements('tr')
+        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows))
+
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать мушку из павлиньих перьев"
         # (Эдит очень методична)
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
