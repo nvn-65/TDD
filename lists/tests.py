b00fae90132a302b0587099f84ade858cd5e69e6
Vladimir <nvn-65@mail.ru>
2021-11-18 21:07:57 +0300

    001

--- lists/tests.py
+++ lists/tests.py
@@ -8,3 +8,9 @@ class HomePageTest(TestCase):
         """тест: используется домашний шаблон"""
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        """тест: можно сохранить post-запрос"""
+        response = self.client.post('/', data={'item_text': 'Новый элемент списка'})
+        self.assertIn('Новый элемент списка', response.content.decode())
+        self.assertTemplateUsed(response, 'home.html')
--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -18,7 +18,13 @@ class NewVisitorTest(unittest.TestCase):
 
     def tearDown(self):
         """демонтаж"""
-        # self.browser.quit()
+        self.browser.quit()
+
+    def check_for_row_in_list_table(self, row_text):
+        """подтверждение строки в таблице списка"""
+        table = self.browser.find_element(By.ID, 'id_list_table')
+        rows = table.find_elements(By.TAG_NAME, 'tr')
+        self.assertIn(row_text, [row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         """тест: можно начать список и получить его позже"""
@@ -43,21 +49,22 @@ class NewVisitorTest(unittest.TestCase):
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
         time.sleep(1)
-
-        table = self.browser.find_element(By.ID, 'id_list_table')
-        rows = table.find_elements(By.TAG_NAME, 'tr')
-        self.assertTrue(any(row.text == '1: Купить павлиньи перья' for row in rows), 'Новый элемент списка не появился '
-                                                                                     'в таблице')
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
-        # Она вводит "Сделать мушку из павлиньих перьев"
-        # (Эдит очень методична)
-        self.fail('Закончить тест!')
+        # Она вводит "Сделать мушку из павлиньих перьев" (Эдит очень методична)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Сделать мушку из павлиньих перьев')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента ее списка
+        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.check_for_row_in_list_table('2: Сделать мушку из павлиньих перьев')
         # Эдит интересно, запомнит ли сайт ее список. Далее она видит, что
         # сайт сгенерировал для нее уникальный URL-адрес – об этом
         # выводится небольшой текст с объяснениями.
+        self.fail('Закончить тест!')
         # Она посещает этот URL-адрес – ее список по-прежнему там.
         # Удовлетворенная, она снова ложится спать
 
