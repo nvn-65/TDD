fbec550c8aea5f63a2201e2c39736f1dadf872e0
Vladimir <nvn-65@mail.ru>
2021-11-20 15:16:28 +0300

    Модель для элементов списка и связанная с ней миграция

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,22 @@
 from django.test import TestCase
+from lists.models import Item
 
+class ItemModelTest(TestCase):
+    """тест модели элемента списка"""
+
+    def test_saving_and_retrieving_items(self):
+        """тест сохранения и получения элементав списка"""
+        first_item = Item()
+        first_item.text = 'Первый (когда-либо) элемент списка'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Пункт второй'
+        second_item.save()
+
+        save_items = Item.objects.all()
+        self.assertEqual(first_item.text, 'Первый (когда-либо) элемент списка')
+        self.assertEqual(second_item.text, 'Пункт второй')
 
 class HomePageTest(TestCase):
     """Тест домашней страницы"""
